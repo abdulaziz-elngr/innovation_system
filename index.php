19fd41ba631f0b7e0014d5cd4053e9be22cdc420
abdulaziz-elngr <eng.abdulaziz510@gmail.com>
2026-01-18 21:43:41 +0200

    تعديل

--- index.php
+++ index.php
@@ -1,3 +1,19 @@
-<?php
-echo"Welcome to Innovation Management System";
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Innovation Library System</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <div class="login-container">
+  <img src="images/logo.png" alt="University Logo">
+  <h2>Innovation Library System</h2>
+  <form action="login.php" method="post">
+    <input type="text" name="email" placeholder="Email" required>
+    <input type="password" name="password" placeholder="Password" required>
+    <button type="submit">Login</button>
+  </form>
+</div>
+</body>
+</html>
