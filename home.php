b94a2a476476d987bf900fe1f529307307b064c9
abdulaziz-elngr <eng.abdulaziz510@gmail.com>
2026-01-24 19:19:53 +0200

    h

--- home.php
+++ home.php
@@ -76,7 +76,7 @@ include('db.php');
         <li><a href="home.php"><i class="fas fa-home"></i> HOME</a></li>
         <li><a href="books.php"><i class="fas fa-book"></i> BOOK</a></li>
         <li><a href="category.php"><i class="fas fa-tags"></i> CATEGORY</a></li>
-        <li><a href="borrowings.php"><i class="fas fa-arrow-right-arrow-left"></i> BORROWING</a></li>
+        <li><a href="borrowing archive.php"><i class="fas fa-arrow-right-arrow-left"></i> BORROWING</a></li>
         <li><a href="student entry archive.php"><i class="fas fa-user-graduate"></i> STUDENT ENTRY</a></li>
       </ul>
     </div>
